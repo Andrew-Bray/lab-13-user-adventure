c159365b4462d1a98356f2e32db1da3831141806
Andrew-Bray <amichaelbray@gmail.com>
2020-10-15 22:50:30 -0700

    a hard day's work with little fruit to show for it

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+//import { findById } from './utils.js';
 // import functions and grab DOM elements
-
+const nameForm = document.getElementById('name-form');
+const speciesForm = document.getElementById('species-form');
 // initialize state
-
+let spider = {};
 // set event listeners to update state and DOM
+nameForm.addEventListener(('submit'), (e) => {
+    e.preventDefault();
+
+    const data = new FormData(nameForm);
+
+    spider = {
+        name: data.get('name'),
+        species: 0,
+        food: 20,
+        hp: 50,
+        completed: {}
+    };
+});
+
+//Species Form Event-listener
+speciesForm.addEventListener(('submit'), (e) => {
+    e.preventDefault();
+    const data = new FormData(speciesForm);
+    spider['species'] = data.get('species');
+});
+
